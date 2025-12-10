7ad7386ef61b6a5108eb00d40ac0306661b1a676
AnaLara <analara714@gmail.com>
2025-12-09 21:31:51 -0300

    Expressoes Lambda

--- HelloWorld-C#/Program.cs
+++ HelloWorld-C#/Program.cs
@@ -6,47 +6,19 @@ using System.Linq; // trabalha com dados e realiza consultas nativas
 using System.Runtime.Intrinsics.X86;
 using System.Text; // trabalha com texto e codificações 
 using System.Threading.Tasks; // construir aplicativos multithreading 
-namespace Generics
+
+namespace LambdaExpressions
 {
-    // Classe genérica: T é um "tipo genérico" que será definido quando instanciado.
-    /* T é um parâmetro de tipo, que só é definido quando você cria o objeto.
-    permitem criar uma única classe que funciona para qualquer tipo, sem duplicar código. */
-    public class GenericList<T>
-    {
-        // Método que recebe um valor do tipo T.
-        public void Add(T value) { }
-
-        // Indexador que retorna um item do tipo T pelo índice.
-        public T this[int index]
-        {
-            get { throw new NotImplementedException(); }
-        }
-    }
-
-    // Classe Book normal (sem generics).
-    public class Book
-    {
-        public void Add(Book book)
-        {
-            throw new NotImplementedException();
-        }
-        public Book this[int index]
-        {
-            get { throw new NotImplementedException(); }
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            // Cria uma lista genérica de inteiros.
-            var numbers = new GenericList<int>();
-            numbers.Add(10);  // Add recebe int, pois T = int.
+          // args => expression
+          // number = number*number
 
-            // Cria uma lista genérica de Books.
-            var book = new GenericList<Book>();
-            book.Add(new Book()); // Add recebe Book, pois T = Book.
+         Func<int, int> square = number => number*number;
+
+            Console.WriteLine(square(5));
         }
     }
 }
